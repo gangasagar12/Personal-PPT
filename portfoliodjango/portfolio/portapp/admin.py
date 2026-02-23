77097a52e4144c184203e8b896e71cbfd107da98
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-02-23 20:41:00 +0545

    manage the models and view of the portapp

--- portfoliodjango/portfolio/portapp/admin.py
+++ portfoliodjango/portfolio/portapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'submitted_at')
+    list_filter = ('submitted_at',)
+    search_fields = ('name', 'email', 'subject')
+    readonly_fields = ('submitted_at',)
--- portfoliodjango/portfolio/portapp/views.py
+++ portfoliodjango/portfolio/portapp/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.conf import settings
+from .form import Contactfrom
  
-def home(request):
-    return render(request, 'home.html')
-
+def contact(request):
+    if request.method=='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('home')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
