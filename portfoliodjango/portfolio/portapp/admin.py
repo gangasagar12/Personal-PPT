b331617fb0ddbd43f6ac9b9ce4aacb68804a4a5e
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-02-23 21:12:01 +0545

    try to fix the bug and recreate the project and app

--- portfoliodjango/portfolio/portapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Contact
-
-@admin.register(Contact)
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'subject', 'submitted_at')
-    list_filter = ('submitted_at',)
-    search_fields = ('name', 'email', 'subject')
-    readonly_fields = ('submitted_at',)
--- portfoliodjango/portolioapp/apps.py
+++ portfoliodjango/portolioapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PortappConfig(AppConfig):
+class PortolioappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portapp'
+    name = 'portolioapp'
